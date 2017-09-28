96f7b32dd850895e01c6939f33fd52cf9d8bd747
troyet <1148948458@qq.com>
2017-09-28 20:21:46 +0800

    git push -u origin master

---
+++ send.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+ <html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="description" content="User login page" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<title>互联网安全平台</title>
+		<link rel="stylesheet" href="assets/css/other.css">
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="assets/css/ace.min.css" />
+</head>
+
+   <div id="navbar" class="navbar navbar-default navbar-fixed-top">
+         <div class="navbar-container" id="navbar-container ">
+           <div class="title" style="font-size: 25px">发布</div>
+
+           <div class="navbar-text pull-left" style="margin-top: -30px; margin-left: 10px; ">
+           <a href="try.php"><span class="glyphicon glyphicon-arrow-left" style="color: rgb(255, 255, 255); font-size:30px "></span></a></div>
+
+         
+
+        </div>
+  </div>
+  <body>
+  <div id="container" clss="main-container" style="padding-top: 80px;">
+    上传图片：
+    <img  src="./assets/css/images/meteorshower2.jpg" style="width: 250px; height: 250px;"><br>
+
+    <div style="float: right;">
+    <input type="button " name="button" value="发表" class="btn btn-primary" style="margin-right: 10px;">
+    </div><br>
+
+
+
+    <div style="float: left;">
+    发表主题
+    </div>
+
+    
+
+
+     <input type="text" name="text" placeholder="请输入标题" style="width: 100%;"><br>
+
+     <input type="text" name="text" placeholder="请输入内容" style="width:100%; height:200px;">
+
+    
+
+
+ 
+
+
+
+
+
+  </div>
+  </body>
+
+
+
+
+
+
+
+
+
+
+
+
+  </html>
--- try.php
+++ try.php
@@ -13,7 +13,16 @@
 </head>
    <div id="navbar" class="navbar navbar-default navbar-fixed-top">
          <div class="navbar-container" id="navbar-container ">
-         <div class="title">曝光台</div> 
+           <div class="title" style="font-size: 25px">曝光台</div>
+
+           <div class="navbar-text pull-left" style="margin-top: -30px; margin-left: 10px; ">
+           <a href="notin.php"><span class="glyphicon glyphicon-arrow-left" style="color: rgb(255, 255, 255); font-size:30px "></span></a></div>
+
+           <div class="navbar-text pull-right" style="margin-top: -30px; margin-right: 10px; ">
+           <a href="send.php"><span class="glyphicon glyphicon-pencil" style="color: rgb(255,255,255); font-size: 27px"></span></a></div>
+
+         
+
         </div>
   </div>
    
@@ -21,28 +30,57 @@
 <div id="main-container" clss="main-container" style="padding-top: 80px;">
     <table class="table">
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     <tr>
-     <td>沐阳分公司某家营业厅大额现金未入柜2017.8.8</td>
+     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
     </tr>
     </table>
-
- 
-
- <div>
+<div>
+<div class="footer">
+			<div class="footer-inner">
+				<div class="footer-content">
+					<ul>
+						<li class="light-red">
+							<a href="note.php" class="shouye">
+								<i class="glyphicon glyphicon-home"></i>
+								<div>工作笔记</div>
+							</a>
+						</li>
+						<li class="grey">
+							<a href="exchange.php" class="weianqi">
+								<i class="glyphicon glyphicon-time"></i>
+								<div>经验交流</div>
+							</a>
+						</li>
+						<li class="grey">
+							<a href="query.php" class="chaxun">
+								<i class="glyphicon glyphicon-search"></i>
+								<div>110</div>
+							</a>
+						</li>
+						<li class="light-blue">
+							<a href="more.php" class="more">
+								<i class="glyphicon glyphicon-user"></i>
+								<div>视频监控</div>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
 
 
