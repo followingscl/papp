0b6e07dc751380ea6b628c8d62aea16bb9de1444
followingscl <2267081957@qq.com>
2017-10-03 10:04:12 +0800

    modified:   announce.php

--- announce.php
+++ announce.php
@@ -21,14 +21,12 @@
 		<div id="scroller">
 		  <div id="main-container" clss="main-container" style="padding-top: 50px;">
 			  <div class="stmm">
-			  <form role="form">
-			    <table class="table">
-			      <tr>
-			        <td style="font-size: 15px;text-align: center;">发表主题</td>
-			        <td style="text-align: center;"><button style="background-color: rgb(107,130,250); color: rgb(255,255,255); border: 2px solid #f5f5f5;">发表</button></td>
-			      </tr>
-			    </table>
-			    <div id="input" style="padding-left: 10px; margin-right: 10px;">
+			  <form class="form">
+			    <div style="padding-left: 20px; padding-right: 20px;margin-top: 20px;">
+             	  <label class="inline">发表主题</label>
+             	  <label class="pull-right"><button onclick="#" style="background-color: rgb(107,130,250);border:1px solid grey;">发表</button></label>
+                 </div> 
+			    <div id="input" style="padding-left: 10px; margin-right: 10px; margin-top: 20px;">
 			    <input type="text" class="form-control" id="theme" placeholder="请输入主题">
 			    <textarea class="form-control" rows="20" placeholder="请输入内容" style="resize: none; margin-top: 20px;"></textarea>
 			    </div>
