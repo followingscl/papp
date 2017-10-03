15a1568fe33bb7c46efc785ece36af78b530acb1
fsmutou <1275574052@qq.com>
2017-10-03 16:57:23 +0800

    try

--- notin.php
+++ notin.php
@@ -1,61 +1,41 @@
-<!DOCTYPE html>
- <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<meta name="description" content="User login page" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-	<title>互联网安全平台</title>
-		<link rel="stylesheet" href="assets/css/other.css">
-
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="assets/css/ace.min.css" />
-</head>
-   <div id="navbar" class="navbar navbar-default navbar-fixed-top">
-         <div class="navbar-container" id="navbar-container ">
-           <div class="title" style="font-size: 25px">大额现金未入柜</div>
-
-           <div class="navbar-text pull-left" style="margin-top: -30px; margin-left: 10px; ">
-           <a href="try.php"><span class="glyphicon glyphicon-arrow-left" style="color: rgb(255, 255, 255); font-size:30px "></span></a></div>
-
-         
-
-        </div>
+<?php include 'template/header.php' ?>
+      <a class="back">
+        <img src="assets/img/back.png" alt="" style="width: 1rem">
+      </a>
+      <script type="text/javascript">
+          var ass = document.getElementsByTagName('a');
+          for(var i = 0;i < ass.length;i ++){
+            if(ass[i].getAttribute('class') == 'back'){
+              var hammertime = new Hammer(ass[i]);
+              hammertime.on("tap", function (ev) {
+                    window.history.go(-1);
+              });
+            }
+          }
+        </script>
+        <a class="edit" style="float: right;
+        padding-top: 3%;padding-right: 3%;position: relative;">
+          <img src="assets/img/edit.png" style="width: 2rem;vertical-align: middle;">
+        </a>
+      <div class="title">大额现金未入柜</div>
+    </div>
   </div>
-   
-<body>
-<div id="main-container" clss="main-container" style="padding-top: 80px;">
-     8月7日沐阳总公司核对账目时发现某家营业厅有五万元现金未入保险柜，询问当班经理和营业员后分别罚款一千元和五百元现金以此警戒
-</div>
+  <div id="wrapper" style="background-color: #fff;">
+    <div id="scroller">
 
-
-<div id="container" style=" padding: 100px 100px 10px;;position: absolute; bottom: 0px;">
-
-    <form class="bs-example bs-example-form" role="form">
-        <div class="input-group">
-            <span class="input-group-addon">☺</span>
-
-            <span class="input-group-btn">
-                        <button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-plus-sign" style="color: rgb(0,0,0);"></button>
-                    </span>
-            
-            <input type="text" class="form-control">
-            <span class="input-group-addon"  ><span class="glyphicon glyphicon-signal"></span></span>
-            
-            
-
-        </div>
-    </form>
-</div>
-</div>
-
-
-  
-
-
-
-
-
+      <div id="main-container" clss="main-container" style="padding-top: 50px;">
+          <h4 style="margin:15px;line-height:30px;text-align:center;">沐阳分公司某家营业厅大额现金未入柜</h3>
+          
+          <p style="margin:30px 15px;line-height: 30px;font-size:15px;">
+           &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp8月7日沐阳总公司核对账目时发现某家营业厅有五万元现金未入保险柜，询问当班经理和营业员后分别罚款一千元和五百元现金以此警戒。
+          </p>
+          <time style="float: right;margin:15px;">2017/8/8</time>
+      </div>
+    </div>
+  </div>
+  <?php 
+    include 'template/footer.php'
+  ?>
 
 </body>
 </html>
--- try.php
+++ try.php
@@ -1,107 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<meta name="description" content="User login page" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-	<title>互联网安全平台</title>
-		<link rel="stylesheet" href="assets/css/other.css">
-
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="assets/css/ace.min.css" />
-</head>
-   <div id="navbar" class="navbar navbar-default navbar-fixed-top">
-         <div class="navbar-container" id="navbar-container ">
-           <div class="title" style="font-size: 25px">曝光台</div>
-
-           <div class="navbar-text pull-left" style="margin-top: -30px; margin-left: 10px; ">
-           <a href="notin.php"><span class="glyphicon glyphicon-arrow-left" style="color: rgb(255, 255, 255); font-size:30px "></span></a></div>
-
-           <div class="navbar-text pull-right" style="margin-top: -30px; margin-right: 10px; ">
-           <a href="send.php"><span class="glyphicon glyphicon-pencil" style="color: rgb(255,255,255); font-size: 27px"></span></a></div>
-
-         
-
-        </div>
+<?php include 'template/header.php' ?>
+			<a class="back">
+				<img src="assets/img/back.png" alt="" style="width: 1rem">
+			</a>
+			<script type="text/javascript">
+					var ass = document.getElementsByTagName('a');
+					for(var i = 0;i < ass.length;i ++){
+						if(ass[i].getAttribute('class') == 'back'){
+							var hammertime = new Hammer(ass[i]);
+		          hammertime.on("tap", function (ev) {
+		              	window.history.go(-1);
+		          });
+						}
+					}
+				</script>
+				<a class="edit" style="float: right;
+				padding-top: 3%;padding-right: 3%;position: relative;">
+				  <img src="assets/img/edit.png" style="width: 2rem;vertical-align: middle;">
+				</a>
+			<div class="title">曝光台</div>
+		</div>
   </div>
-   
-<body>
-<div id="main-container" clss="main-container" style="padding-top: 80px;">
-    <table class="table">
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    <tr>
-     <td><a href="notin.php">沐阳分公司某家营业厅大额现金未入柜      2017.8.8</a></td>
-    </tr>
-    </table>
-<div>
-<div class="footer">
-			<div class="footer-inner">
-				<div class="footer-content">
-					<ul>
-						<li class="light-red">
-							<a href="note.php" class="shouye">
-								<i class="glyphicon glyphicon-home"></i>
-								<div>工作笔记</div>
-							</a>
-						</li>
-						<li class="grey">
-							<a href="exchange.php" class="weianqi">
-								<i class="glyphicon glyphicon-time"></i>
-								<div>经验交流</div>
-							</a>
-						</li>
-						<li class="grey">
-							<a href="query.php" class="chaxun">
-								<i class="glyphicon glyphicon-search"></i>
-								<div>110</div>
-							</a>
-						</li>
-						<li class="light-blue">
-							<a href="more.php" class="more">
-								<i class="glyphicon glyphicon-user"></i>
-								<div>视频监控</div>
-							</a>
-						</li>
-					</ul>
-				</div>
+  <div id="wrapper" style="background-color: #fff;">
+		<div id="scroller">
+			<div id="main-container" clss="main-container" style="padding-top: 50px;">
+				<ul id="try-ul">
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    <li id="try-list">
+				    	<a href="notin.php"><mark id="try-mark">沐阳分公司某家营业厅大额现金未入柜</mark> <span id="try-span"><img src="assets/img/right.png"></span><time id="try-time">2017/8/8</time></a>
+				    </li>
+				    
+			    </ul>
+
 			</div>
 		</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	</div>
+  <?php 
+		include 'template/footer.php'
+	?>
 
 </body>
 </html>
