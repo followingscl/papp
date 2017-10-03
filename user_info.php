54984f4a438376b8d55b2b8b16190256f1b16f1b
followingscl <2267081957@qq.com>
2017-10-03 09:31:30 +0800

    new file:   assets/img/to.png

--- user_info.php
+++ user_info.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<meta name="description" content="User login page" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-	<title>互联网安全平台</title>
-		<link rel="stylesheet" href="assets/css/other.css">
-
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="assets/css/ace.min.css" />
-</head>
-<body>
-  <div id="navbar" class="navbar navbar-default navbar-fixed-top">
-			<div class="navbar-container" id="navbar-container">
+<?php include 'template/header.php' ?>
 			<a class="back">
 				<img src="assets/img/back.png" alt="" style="width: 1rem">
 			</a>
@@ -28,81 +13,60 @@
 						}
 					}
 				</script>
+				<a style="float: right;
+				padding-top: 3%;padding-right: 3%;position: relative; color: #ffffff;font-size: 18px;">
+				 保存
+				</a>
 			<div class="title">个人信息</div>
-			<div class="navbar-text pull-right" style="margin-top: -30px; margin-right: 10px; ">保存</div>
-			</div>
-  </div>
-  <div id="main-container" clss="main-container" style="padding-top: 80px;">
-    <table class="table">
-      <tr>
-        <td><img src="" width="50px" height="50px"></td>
-        <td>修改头像</td>
-      </tr>
-      <tr>
-        <td>昵称</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>职位</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>隶属</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>单位</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>联系方式</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>修改密码</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>版本更新</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>关于我们</td>
-        <td></td>
-      </tr>
-    </table>
-  </div>
-  <div class="footer">
-			<div class="footer-inner">
-				<div class="footer-content">
-					<ul>
-						<li class="light-red">
-							<a href="note.php" class="shouye">
-								<i class="glyphicon glyphicon-home"></i>
-								<div>工作笔记</div>
-							</a>
-						</li>
-						<li class="grey">
-							<a href="exchange.php" class="weianqi">
-								<i class="glyphicon glyphicon-time"></i>
-								<div>经验交流</div>
-							</a>
-						</li>
-						<li class="grey">
-							<a href="query.php" class="chaxun">
-								<i class="glyphicon glyphicon-search"></i>
-								<div>110</div>
-							</a>
-						</li>
-						<li class="light-blue">
-							<a href="more.php" class="more">
-								<i class="glyphicon glyphicon-user"></i>
-								<div>视频监控</div>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
 		</div>
+  </div>
+  <div id="wrapper">
+		<div id="scroller">
+		  <div id="main-container" clss="main-container" style="padding-top: 70px; padding-left: 10px; padding-right: 10px;">
+		   
+           <ul class="list-group">
+             <li class="list-group-item" onclick="javascript:window.location.href='alterphoto.php'">
+             	<label class="inline"><img src=""></label>
+             	<label class="pull-right" style="vertical-align: middle;">修改头像</label>
+             </li>
+             <li class="list-group-item">
+                <label class="inline">昵称</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item">
+             	<label class="inline">职位</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item">
+               <label class="inline">隶属</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item">
+             	<label class="inline">单位</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item">
+              <label class="inline">联系方式</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item" onclick="javascript:window.location.href='alterpassword.php'">
+              <label class="inline">修改密码</label>
+             	<label class="pull-right"><img src="./assets/img/to.png" style="width:20px;height: 20px;"></label>
+             </li>
+             <li class="list-group-item">
+              <label class="inline">版本更新</label>
+             	<label class="pull-right"></label>
+             </li>
+             <li class="list-group-item" onclick="javascript:window.location.href='aboutus.php'">
+              <label class="inline">关于我们</label>
+             </li>
+           </ul>
+
+		  </div>
+		</div>
+	</div>
+  <?php 
+		include 'template/footer.php'
+	?>
 </body>
 </html>
