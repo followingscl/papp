6bfb3e89ac76f6fc32adea1b987316c883df4251
fsmutou <1275574052@qq.com>
2017-10-09 18:24:32 +0800

    try

--- exchange.php
+++ exchange.php
@@ -20,15 +20,15 @@
 			<div class="title">经验交流</div>
 		</div>
   </div>
-  <div id="wrapper" style="background-color: #fff;">
+  <div id="wrapper" style="background-color: #f2f2f2;">
 		<div id="scroller">
-		  <div id="main-container" clss="main-container" style="padding-top: 70px; padding-left: 10px; padding-right: 10px;">
-		    <div>
-				<h5 style="margin: 5px;">如何让营业员快速掌握“三要两会”口号</h4>
+		  <div id="main-container" clss="main-container" style="padding-top: 40px; ">
+		    <div style="background-color: #fff;height: 125px;padding:15px;border:5px double #eee;margin:10px 0;background-image:url(assets/img/timg.jpg);background-size:cover;">
+				<h5 style="margin: 5px;"><b>如何让营业员快速掌握“三要两会”口号</b></h5>
 				<time  style="float:right;margin: 5px;">2017/8/6</time><br>
 				<div style="margin:10px;">
-					<button style="background-color: #fff; color: rgb(156,209,247); border-style:none;height: 28px;width:63px;text-align:center;margin:5px 40px;float: left;">回复<img src="assets/img/reply.png" style="height: 25px;width: 25px;"></button>
-					<button style="background-color: rgb(156,209,247); color: #fff; border: 1px solid #e0e0e0; border-radius:4px;height: 28px;width:63px;text-align:center;margin:5px 40px;float:right;">分享<img src="assets/img/share.png" style="height: 20px;width: 20px;"></button>
+					<button style="background-color: #fff; color: rgb(156,209,247); border-style:none;border-radius:4px;height: 28px;width:63px;text-align:center;margin:5px 40px;float: left;">回复<img src="assets/img/reply.png" style="height: 25px;width: 25px;"></button>
+					<button style="background-color: rgb(156,209,247); color: #fff; border-style:none;border-radius:4px;height: 28px;width:63px;text-align:center;margin:5px 40px;float:right;">分享<img src="assets/img/share.png" style="height: 20px;width: 20px;"></button>
 				</div>
 		    </div>
 		  </div>
