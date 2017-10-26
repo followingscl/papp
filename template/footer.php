f256cca323a704b04ec03ef2cd163ec72f841f37
amenging <1508235075@qq.com>
2017-10-26 17:00:17 +0800

    all

--- template/footer.php
+++ template/footer.php
@@ -36,7 +36,7 @@
 	<script src="assets/js/jquery.hammer.js" type="text/javascript"></script>
 		<script type="text/javascript">
 			$(function(){
-				$('document').on('tap click pan', function() {
+				$('.main-container').on('tap click pan', function() {
 					// console.log($('.form-control:focus'))
 					alert('refre')
 					$('.form-control:focus')?$('.form-control:focus').blur():''
